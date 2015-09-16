5e3d2724fa5a4b24da036fce0eebdf74fa4df886
Mio Nilsson <mio@dice.fm>
2015-09-16 08:57:35 +0100

    Tweaks

--- zsh/custom/dev_paths.zsh
+++ zsh/custom/dev_paths.zsh
@@ -3,6 +3,9 @@ DEV_PATH=~/src
 dev() { cd $DEV_PATH/$1; }
 dgo() { cd $DEV_PATH/GO_Dev/$1; }
 
+autoload -U compinit compdef
+compinit
+
 _dev() { _files -W $DEV_PATH -/; }
 _dgo() { _files -W $DEV_PATH/GO_Dev -/; }
 compdef _dev dev
