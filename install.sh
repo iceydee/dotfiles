23b2d94fb467ceff0343459d0f91ee9fc31a8354
Mio Nilsson <mionilsson@gmail.com>
2016-07-01 10:04:48 +0200

    Tmux config

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 ln -sf ~/my/dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/my/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/my/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 for FILE in `find ~/my/dotfiles/zsh/custom -type f`; do
   ln -sf ${FILE} ~/.oh-my-zsh/custom/`basename ${FILE}`
 done
