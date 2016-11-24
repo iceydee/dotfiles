a5e6db186b395ac5d59fbed1f325b9b46734b9db
Mio Nilsson <mio@dice.fm>
2016-11-24 06:34:50 +0100

    :sparkles: Switch between different profiles

--- task/install.sh
+++ task/install.sh
@@ -4,3 +4,4 @@ ln -sf ${HOME}/my/dotfiles/task/.taskrc ${HOME}/.taskrc
 ln -sf ${HOME}/my/dotfiles/task/task.theme ${HOME}/.task.theme
 sudo ln -sf ${HOME}/my/dotfiles/task/tf /usr/local/bin/tf
 sudo ln -sf ${HOME}/my/dotfiles/task/f /usr/local/bin/f
+sudo ln -sf ${HOME}/my/dotfiles/task/taskswitch /usr/local/bin/taskswitch
