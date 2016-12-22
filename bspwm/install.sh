c5095098fbfb0e4621f5c706b49e0865acd0c109
Mio Nilsson <mionilsson@gmail.com>
2016-12-22 08:43:40 +0100

    :sparkles: Multi-monitor switch desktop

--- bspwm/install.sh
+++ bspwm/install.sh
@@ -56,3 +56,4 @@ ln -sf ~/my/dotfiles/bspwm/startup ~/.config/bspwm/startup
 ln -sf ~/my/dotfiles/bspwm/task_panel ~/.config/bspwm/task_panel
 sudo ln -sf ~/my/dotfiles/bspwm/focus /usr/local/bin/focus
 sudo cp ~/my/dotfiles/bspwm/bspwm.desktop /usr/share/xsessions/
+sudo ln -sf ~/my/dotfiles/bspwm/.desktop /usr/local/bin/.desktop
