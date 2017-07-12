caa1b02105d652bcf484150e15dfb8b709a956c8
Mio Nilsson <mionilsson@gmail.com>
2017-07-12 10:01:32 +0200

    :art: Tweaks

--- zsh/custom/env_config.zsh
+++ zsh/custom/env_config.zsh
@@ -11,4 +11,7 @@ export GOPATH=$DEV_PATH/GO_Dev
 export DICE_REDIS_HOST_AND_PORT="127.0.0.1:6379"
 export DICE_REDIS_PASSWORD=""
 
+export JAVA_HOME=/usr/lib/jvm/default-java/jre
+export PATH=$PATH:${JAVA_HOME}/bin
+
 . ~/.nvm/nvm.sh
