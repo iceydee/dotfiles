b9a745c37c5c4e71253607c6e75ab7528dddb8f0
Mio Nilsson <mionilsson@gmail.com>
2017-07-13 09:08:53 +0200

    :bug: Add missing vte-ng dependency

--- termite/install.sh
+++ termite/install.sh
@@ -5,7 +5,7 @@ cd ~/src/github.com/thestinger
 
 git clone --recursive https://github.com/thestinger/termite.git
 git clone https://github.com/thestinger/vte-ng.git
-sudo apt-get install g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac
+sudo apt-get install g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac intltool
 sudo apt-get install libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev
 sudo apt-get install libxml2-utils gperf libpcre2-dev
 cd vte-ng && git checkout 0.46.1-ng && ./autogen.sh && make && sudo make install
