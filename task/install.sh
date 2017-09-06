83867a460e1bdec53056d8d9ad06c046ed5d0897
Mio Nilsson <mionilsson@gmail.com>
2017-09-06 08:53:22 +0200

    :sparkles: Task lists

--- task/install.sh
+++ task/install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo apt-get -y install taskwarrior
+sudo apt-get -y install taskwarrior jq
 
+mkdir -p "${HOME}/.tasklists"
 ln -sf ${HOME}/my/dotfiles/task/.taskrc ${HOME}/.taskrc
 ln -sf ${HOME}/my/dotfiles/task/task.theme ${HOME}/.task.theme
+sudo ln -sf ${HOME}/my/dotfiles/task/tl /usr/local/bin/tl
 sudo ln -sf ${HOME}/my/dotfiles/task/tf /usr/local/bin/tf
 sudo ln -sf ${HOME}/my/dotfiles/task/f /usr/local/bin/f
 sudo ln -sf ${HOME}/my/dotfiles/task/taskswitch /usr/local/bin/taskswitch
