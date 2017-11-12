e794c59d901650201538e34e8c273c3a08a849f6
Mio <mio@plingot.com>
2017-11-12 11:41:50 +0100

    :art: Better support for osx

--- zsh/custom/zsh-config.zsh
+++ zsh/custom/zsh-config.zsh
@@ -2,4 +2,9 @@ RPROMPT="\$(~/.rvm/bin/rvm-prompt s i v g)%{$fg[yellow]%}[%*]"
 
 DEFAULT_USER="mio"
 
-eval `dircolors ~/.solarized/dircolors.ansi-dark`
+if whence dircolors >/dev/null; then
+  eval `dircolors ~/.solarized/dircolors.ansi-dark`
+else
+  export CLICOLOR=1
+  export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
+fi
