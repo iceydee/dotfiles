12c3dd5314d97fbed801cb8fde8d8c38c50498ab
Mio Nilsson <mio@plingot.com>
2018-01-22 20:44:10 +0100

    :art: Tweaks to path env

--- zsh/custom/env_config.zsh
+++ zsh/custom/env_config.zsh
@@ -12,6 +12,6 @@ export DICE_REDIS_HOST_AND_PORT="127.0.0.1:6379"
 export DICE_REDIS_PASSWORD=""
 
 export JAVA_HOME=/usr/lib/jvm/default-java/jre
-export PATH=$PATH:${JAVA_HOME}/bin
+export PATH=$PATH:${JAVA_HOME}/bin:${HOME}/.local/bin
 
 . ~/.nvm/nvm.sh
