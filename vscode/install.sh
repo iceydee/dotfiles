54e5365296903e81af7dcd98de3c11b728d50d72
Mio <mio@plingot.com>
2018-01-27 07:15:25 +0100

    :art: VSCode config for osx

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-mkdir -p ~/.config/Code/User
-ln -sf ~/my/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+if [ -z "$(uname -s | grep "Darwin")" ]; then
+  mkdir -p ~/.config/Code/User
+  ln -sf ~/my/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+else
+  mkdir -p ~/Library/Application\ Support/Code/User
+  ln -sf ~/my/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+fi
