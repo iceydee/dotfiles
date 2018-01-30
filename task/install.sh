2d7f91ed68081592bf4388e8c524d2a85029c7ac
Mio <mio@plingot.com>
2018-01-30 10:41:12 +0100

    :whale: Fix sudo issue for docker

--- task/install.sh
+++ task/install.sh
@@ -8,7 +8,14 @@ fi
 mkdir -p "${HOME}/.tasklists"
 ln -sf ${HOME}/my/dotfiles/task/.taskrc ${HOME}/.taskrc
 ln -sf ${HOME}/my/dotfiles/task/task.theme ${HOME}/.task.theme
-sudo ln -sf ${HOME}/my/dotfiles/task/tl /usr/local/bin/tl
-sudo ln -sf ${HOME}/my/dotfiles/task/tf /usr/local/bin/tf
-sudo ln -sf ${HOME}/my/dotfiles/task/f /usr/local/bin/f
-sudo ln -sf ${HOME}/my/dotfiles/task/taskswitch /usr/local/bin/taskswitch
+
+# We're already root in docker
+SUDO="sudo"
+if [ "${UID}" -eq 0 ]; then
+  SUDO=""
+fi
+
+${SUDO} ln -sf ${HOME}/my/dotfiles/task/tl /usr/local/bin/tl
+${SUDO} ln -sf ${HOME}/my/dotfiles/task/tf /usr/local/bin/tf
+${SUDO} ln -sf ${HOME}/my/dotfiles/task/f /usr/local/bin/f
+${SUDO} ln -sf ${HOME}/my/dotfiles/task/taskswitch /usr/local/bin/taskswitch
