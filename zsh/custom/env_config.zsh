4dc93e1fb1705936e71780f705a64da7ef03400f
Mio Nilsson <mio@plingot.com>
2018-02-06 16:14:15 +0100

    :art: Use file backend for aws-vault

--- zsh/custom/env_config.zsh
+++ zsh/custom/env_config.zsh
@@ -9,6 +9,6 @@ export NODE_ENV=development
 export JAVA_HOME=/usr/lib/jvm/default-java/jre
 export PATH=$PATH:${JAVA_HOME}/bin:${HOME}/.local/bin
 
-export AWS_VAULT_BACKEND=secret-service
+export AWS_VAULT_BACKEND=file
 
 . ~/.nvm/nvm.sh
