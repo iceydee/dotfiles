4633a9843504576986f8899f6a76c1aff51e9051
Mio Nilsson <mio@plingot.com>
2018-02-09 11:45:41 +0100

    :sparkles: Freshsales integration for taskwarrior

--- task/install.sh
+++ task/install.sh
@@ -8,6 +8,7 @@ fi
 mkdir -p "${HOME}/.tasklists"
 ln -sf ${HOME}/my/dotfiles/task/.taskrc-plingot ${HOME}/.taskrc
 ln -sf ${HOME}/my/dotfiles/task/task.theme ${HOME}/.task.theme
+ln -sf ${HOME}/my/dotfiles/task/on-modify ${HOME}/Dropbox/PlingotTask/hooks/on-modify
 
 # We're already root in docker
 SUDO="sudo"
@@ -19,3 +20,4 @@ ${SUDO} ln -sf ${HOME}/my/dotfiles/task/tl /usr/local/bin/tl
 ${SUDO} ln -sf ${HOME}/my/dotfiles/task/tf /usr/local/bin/tf
 ${SUDO} ln -sf ${HOME}/my/dotfiles/task/f /usr/local/bin/f
 ${SUDO} ln -sf ${HOME}/my/dotfiles/task/taskswitch /usr/local/bin/taskswitch
+${SUDO} ln -sf ${HOME}/my/dotfiles/task/pull-tasks /usr/local/bin/pull-tasks
