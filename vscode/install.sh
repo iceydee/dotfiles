7756ff7a3361942711b41ef73900095ba64e1406
Mio Nilsson <mio@plingot.com>
2018-03-21 07:36:37 +0100

    :sparkles: Install vscode extensions

--- vscode/install.sh
+++ vscode/install.sh
@@ -7,3 +7,15 @@ else
   mkdir -p ~/Library/Application\ Support/Code/User
   ln -sf ~/my/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 fi
+
+CODE=$(which code)
+if [ -n "${CODE}" ]; then
+  EXT="${HOME}/my/dotfiles/vscode/extensions"
+  A=0
+  LINES=$(wc -l ${EXT} | awk '{print $1}')
+  while [ "${A}" -lt "${LINES}" ]; do
+    let "A+=1"
+    code --install-extension $(head -n${A} ${EXT} | tail -n1)
+  done
+fi
+
