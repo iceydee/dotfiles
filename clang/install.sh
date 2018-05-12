552d5abf044053a4372e992eeb80f80955c4b6df
Mio Nilsson <mio@plingot.com>
2018-05-12 12:02:28 +0200

    :art: Update-alternatives for clang-format

--- clang/install.sh
+++ clang/install.sh
@@ -4,5 +4,6 @@ sudo apt install -y clang-6.0 clang-format
 
 sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-6.0 100
 sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-6.0 100
+sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-6.0 100
 sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
 sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
