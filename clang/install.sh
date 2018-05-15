9a49c9a1982dddab51bc8dd0d9d4aac91ab171a6
Mio Nilsson <mio@plingot.com>
2018-05-15 12:11:44 +0200

    :art: Tweak lldb install

--- clang/install.sh
+++ clang/install.sh
@@ -8,3 +8,4 @@ sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/c
 sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
 sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
 sudo update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-6.0 100
+sudo update-alternatives --install /usr/bin/lldb-mi lldb-mi /usr/bin/lldb-mi-6.0 100
