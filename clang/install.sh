66fd859c2130e7e9541dd9e7948cd6dd34e2e70c
Mio Nilsson <mio@plingot.com>
2018-05-15 12:06:17 +0200

    :sparkles: Install lldb as well

--- clang/install.sh
+++ clang/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sudo apt install -y clang-6.0 clang-format
+sudo apt install -y clang-6.0 clang-format lldb-6.0
 
 sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-6.0 100
 sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-6.0 100
 sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-6.0 100
 sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
 sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
+sudo update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-6.0 100
