baeefba074d1d86164c8dcf1e9559eab4b4427f3
Mio Nilsson <iceydee@users.noreply.github.com>
2019-04-01 11:48:34 +0200

    :art: Install lld-7 as well

--- clang/install.sh
+++ clang/install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-sudo apt install -y clang-7 clang-format-7 lldb-7
+sudo apt install -y clang-7 clang-format-7 lldb-7 lld-7
 
 sudo update-alternatives --remove-all clang++
 sudo update-alternatives --remove-all clang
 sudo update-alternatives --remove-all clang-format
 sudo update-alternatives --remove-all c++
 sudo update-alternatives --remove-all cc
+sudo update-alternatives --remove-all lld
 sudo update-alternatives --remove-all lldb
 sudo update-alternatives --remove-all lldb-mi
 sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-7 100
@@ -14,5 +15,6 @@ sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-7 100
 sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-7 100
 sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
 sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
+sudo update-alternatives --install /usr/bin/lld lld /usr/bin/lld-7 100
 sudo update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-7 100
 sudo update-alternatives --install /usr/bin/lldb-mi lldb-mi /usr/bin/lldb-mi-7 100
