e87efe23447e0cb4a748ac9ef74c615e7dd40c25
Mio Nilsson <iceydee@users.noreply.github.com>
2019-04-22 07:51:59 +0200

    :art: Use clang-8

--- clang/install.sh
+++ clang/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y clang-7 clang-format-7 lldb-7 lld-7
+sudo apt install -y clang-8 clang-format-8 lldb-8 lld-8
 
 sudo update-alternatives --remove-all clang++
 sudo update-alternatives --remove-all clang
@@ -10,11 +10,11 @@ sudo update-alternatives --remove-all cc
 sudo update-alternatives --remove-all lld
 sudo update-alternatives --remove-all lldb
 sudo update-alternatives --remove-all lldb-mi
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-7 100
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-7 100
-sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-7 100
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-8 100
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-8 100
+sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-8 100
 sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
 sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
-sudo update-alternatives --install /usr/bin/lld lld /usr/bin/lld-7 100
-sudo update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-7 100
-sudo update-alternatives --install /usr/bin/lldb-mi lldb-mi /usr/bin/lldb-mi-7 100
+sudo update-alternatives --install /usr/bin/lld lld /usr/bin/lld-8 100
+sudo update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-8 100
+sudo update-alternatives --install /usr/bin/lldb-mi lldb-mi /usr/bin/lldb-mi-8 100
