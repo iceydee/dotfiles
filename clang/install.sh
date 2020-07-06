8b079e82e2185838869124f4d5057c463ab1ae37
Mio Nilsson <mio@plingot.com>
2020-07-06 17:48:32 +0200

    :art: Updated to clang-10

--- clang/install.sh
+++ clang/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y clang-8 clang-format-8 lldb-8 lld-8
+sudo apt install -y clang-10 clang-format-10 lldb-10 lld-10
 
 sudo update-alternatives --remove-all clang++
 sudo update-alternatives --remove-all clang
@@ -10,11 +10,11 @@ sudo update-alternatives --remove-all cc
 sudo update-alternatives --remove-all lld
 sudo update-alternatives --remove-all lldb
 sudo update-alternatives --remove-all lldb-mi
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-8 100
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-8 100
-sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-8 100
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-10 100
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-10 100
+sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-10 100
 sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
 sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
-sudo update-alternatives --install /usr/bin/lld lld /usr/bin/lld-8 100
-sudo update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-8 100
-sudo update-alternatives --install /usr/bin/lldb-mi lldb-mi /usr/bin/lldb-mi-8 100
+sudo update-alternatives --install /usr/bin/lld lld /usr/bin/lld-10 100
+sudo update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-10 100
+sudo update-alternatives --install /usr/bin/lldb-mi lldb-mi /usr/bin/lldb-mi-10 100
