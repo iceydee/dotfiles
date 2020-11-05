34180fb4329d59f05a90cf18df57dfca3bff266d
Mio Nilsson <mio@plingot.com>
2020-11-05 15:48:34 +0100

    :art: Install Kops

--- aws/install.sh
+++ aws/install.sh
@@ -40,6 +40,15 @@ if [ -z "$(which aws-iam-authenticator | grep -v "not found")" ]; then
   fi
 fi
 
+# KOPS
+if [ -z "$(which kops | grep -v "not found")"]; then
+  if [ "$(uname -s)" = "Linux" ]; then
+    curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+    chmod +x kops-linux-amd64
+    sudo mv kops-linux-amd64 /usr/local/bin/kops
+  fi
+fi
+
 # Kubectl
 if [ -z "$(which kubectl | grep -v "not found")" ]; then
   if [ "$(uname -s)" = "Linux" ]; then
