dbf5bf9e2b3cd1470cf647692a5b0d64a4dc3ed2
Mio Nilsson <mio@plingot.com>
2020-11-11 09:15:59 +0100

    :art: Updates

--- aws/install.sh
+++ aws/install.sh
@@ -41,7 +41,7 @@ if [ -z "$(which aws-iam-authenticator | grep -v "not found")" ]; then
 fi
 
 # KOPS
-if [ -z "$(which kops | grep -v "not found")"]; then
+if [ -z "$(which kops | grep -v "not found")" ]; then
   if [ "$(uname -s)" = "Linux" ]; then
     curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
     chmod +x kops-linux-amd64
