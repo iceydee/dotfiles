b4b8b983088273c32cdb1a77bb9deb7888196e2c
Mio Nilsson <mio@plingot.com>
2021-01-19 11:18:11 +0100

    :art: Also install libc++ and libc++abi

--- clang/install.sh
+++ clang/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-sudo apt install -y cmake clang-11 clang-format-11 lldb-11 lld-11
+sudo apt install -y \
+cmake \
+clang-11 \
+clang-format-11 \
+lldb-11 \
+lld-11 \
+libc++-11-dev \
+libc++abi-11-dev
 
 ./clang/set_clang.sh
