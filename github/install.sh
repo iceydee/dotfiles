2e8114a15ae5dd1561db1b1e39674b199efd8fdb
Mio Nilsson <mio@plingot.com>
2021-05-04 13:42:27 +0200

    :art: Updated git config

--- github/install.sh
+++ github/install.sh
@@ -12,7 +12,10 @@ sudo make install
 
 git config --global user.email "mio@plingot.com"
 git config --global user.name "Mio Nilsson"
+git config --global user.signingkey "3F6F72864F247293"
 git config --global merge.tool vscode
 git config --global mergetool.vscode.cmd "code --wait \$MERGED"
 git config --global diff.tool vscode
 git config --global difftool.vscode.cmd "code --wait --diff \$LOCAL \$REMOTE"
+git config --global commit.gpgSign "true"
+git config --global hub.protocol "ssh"
