efa2e967d720037c42c40e242d9aeb5415fa75ab
Mio Nilsson <iceydee@users.noreply.github.com>
2022-03-28 13:38:32 +0200

    :art: Install aws-iam-authenticator

--- aws/install.sh
+++ aws/install.sh
@@ -115,6 +115,8 @@ if [ -z "$(which kops | grep -v "not found")" ]; then
     curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
     chmod +x kops-linux-amd64
     sudo mv kops-linux-amd64 /usr/local/bin/kops
+    elif [ "$(uname -s)" = "Darwin" ]; then
+    brew install aws-iam-authenticator
   fi
 fi
 
