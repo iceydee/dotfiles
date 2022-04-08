6a9d8acaa6fb4a630e889914043ed44a43f8c2e9
Mio Nilsson <mio@plingot.com>
2022-04-08 06:42:34 +0200

    :art: gprune command

--- zsh/custom/dev_paths.zsh
+++ zsh/custom/dev_paths.zsh
@@ -10,3 +10,16 @@ _dev() { _files -W $DEV_PATH -/; }
 _dgo() { _files -W $DEV_PATH/GO_Dev -/; }
 compdef _dev dev
 compdef _dgo dgo
+
+
+gprune() {
+  LOCAL=$(mktemp)
+  REMOTE=$(mktemp)
+  git branch | sed 's|* |  |' | sort > "${LOCAL}"
+  git branch -r | sed 's|origin/||' | sort > "${REMOTE}"
+  comm -23 "${LOCAL}" "${REMOTE}" | aws '{print $1}' | xargs git branch -D
+  rm -f "${LOCAL}"
+  rm -f "${REMOTE}"
+}
+
+
