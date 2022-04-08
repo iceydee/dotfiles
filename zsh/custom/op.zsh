d1d0a4e71adab9a63a6a8cc5f7e8bb481dd86117
Mio Nilsson <mio@plingot.com>
2022-04-08 05:48:30 +0200

    :sparkles: op commands

---
+++ zsh/custom/op.zsh
@@ -0,0 +1,31 @@
+function ensureSignIn {
+  op user get mio@plingot.com > /dev/null 2>&1
+  if [ "$?" -ne 0 ]; then
+    eval $(op signin)
+  fi
+}
+
+function ops {
+  local QUERY
+  local TAGS
+  local VAULT
+
+  if [ -z "${1}" ]; then
+    echo "usage: ${0} [query] (tags) (vault)"
+    return -1
+  fi
+
+  ensureSignIn
+
+  QUERY="${1}"
+  TAGS="${2}"
+  VAULT="${3:-Personal}"
+
+  op item list --vault "${VAULT}" --tags "${TAGS}" --format=json | jq -r ".[] | select(.title | ascii_downcase | contains(\"${QUERY}\")).id"
+}
+
+function opc {
+  ensureSignIn
+  local ID=$(ops $@)
+  op item get "${ID}" --fields password | jq -r | pbcopy
+}
