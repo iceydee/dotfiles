5653c5b79fec8ba279c62c7ab867dfdd0e17277e
Mio Nilsson <mio@plingot.com>
2022-04-08 05:54:37 +0200

    :art: Full result

--- zsh/custom/op.zsh
+++ zsh/custom/op.zsh
@@ -24,6 +24,25 @@ function ops {
   op item list --vault "${VAULT}" --tags "${TAGS}" --format=json | jq -r ".[] | select(.title | ascii_downcase | contains(\"${QUERY}\")).id"
 }
 
+function opf {
+  local QUERY
+  local TAGS
+  local VAULT
+
+  if [ -z "${1}" ]; then
+    echo "usage: ${0} [query] (tags) (vault)"
+    return -1
+  fi
+
+  ensureSignIn
+
+  QUERY="${1}"
+  TAGS="${2}"
+  VAULT="${3:-Personal}"
+
+  op item list --vault "${VAULT}" --tags "${TAGS}" --format=json | jq -r ".[] | select(.title | ascii_downcase | contains(\"${QUERY}\"))"
+}
+
 function opc {
   ensureSignIn
   local ID=$(ops $@)
