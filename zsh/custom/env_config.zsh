c94791dcf3df4a7ecf586ca8e3c2318557f01d04
Mio Nilsson <mio@plingot.com>
2023-02-07 10:06:35 +0100

    :art: Android studio

--- zsh/custom/env_config.zsh
+++ zsh/custom/env_config.zsh
@@ -7,7 +7,7 @@ export RACK_ENV=development
 export NODE_ENV=development
 
 export JAVA_HOME=/usr/lib/jvm/default-java/jre
-export PATH=$PATH:${JAVA_HOME}/bin:${HOME}/.local/bin
+export PATH=$PATH:${JAVA_HOME}/bin:${HOME}/.local/bin:${HOME}/android-studio/bin
 
 export AWS_VAULT_BACKEND=file
 
