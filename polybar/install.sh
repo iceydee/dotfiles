f64398a3877f99d853cce11cf6c35e873ed536c6
Mio Nilsson <mio@plingot.com>
2023-02-20 09:41:08 +0100

    :art: Control mic volume

--- polybar/install.sh
+++ polybar/install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+mkdir -p ~/src
+cd ~/src
+git clone -b v2.0.0 git@github.com:MarcDonald/polybar-mic-volume
+ln -sf "${HOME}"/src/polybar-mic-volume/mic-volume/mic-volume.sh "${HOME}"/.local/bin/mic-volume.sh
+
 mkdir -p ~/.config/polybar
 ln -sf ~/my/dotfiles/polybar/config ~/.config/polybar/config
