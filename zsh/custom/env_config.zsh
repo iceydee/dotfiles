266cf13b9f264af4190898e36167f8e695077479
Mio Nilsson <mio@plingot.com>
2023-09-18 10:35:05 +0200

    :art: Python config for gcloud

--- zsh/custom/env_config.zsh
+++ zsh/custom/env_config.zsh
@@ -18,5 +18,7 @@ export AWS_VAULT_BACKEND=file
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/mio/google-cloud-sdk/path.zsh.inc' ]; then . '/home/mio/google-cloud-sdk/path.zsh.inc'; fi
 
+export CLOUDSDK_PYTHON_SITEPACKAGES=1
+
 # The next line enables shell command completion for gcloud.
 if [ -f '/home/mio/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/mio/google-cloud-sdk/completion.zsh.inc'; fi
