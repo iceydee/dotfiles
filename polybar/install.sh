26dce1829578e80c0114ae1c97b31db236246e8e
Mio Nilsson <mio@plingot.com>
2023-10-26 06:23:39 +0200

    :art: Fix polybar install

--- polybar/install.sh
+++ polybar/install.sh
@@ -3,6 +3,7 @@
 mkdir -p ~/src
 cd ~/src
 git clone -b v2.0.0 git@github.com:MarcDonald/polybar-mic-volume
+mkdir -p "${HOME}"/.local/bin
 ln -sf "${HOME}"/src/polybar-mic-volume/mic-volume/mic-volume.sh "${HOME}"/.local/bin/mic-volume.sh
 
 mkdir -p ~/.config/polybar
