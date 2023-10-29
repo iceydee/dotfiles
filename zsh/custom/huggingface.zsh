2b2cdc640d5221c9e63a75cadb92beba3215a10d
Mio Nilsson <mio@plingot.com>
2023-10-29 06:27:12 +0100

    :art: Huggingface config

---
+++ zsh/custom/huggingface.zsh
@@ -0,0 +1,4 @@
+export HF_TOKEN_PATH="/mnt/bigdrive/huggingface/token"
+export HF_DATASETS_CACHE="/mnt/bigdrive/huggingface"
+export HUGGINGFACE_HUB_CACHE="/mnt/bigdrive/huggingface/hub"
+export HUGGINGFACE_ASSETS_CACHE="/mnt/bigdrive/huggingface/assets"
