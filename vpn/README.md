917f41529a48fba1cdfeb45455494ca63bd5dffb
Mio Nilsson <mio@plingot.com>
2023-10-29 20:26:25 +0100

    :art: Fix VPN

--- vpn/README.md
+++ vpn/README.md
@@ -1,4 +1,10 @@
 # VPN Config
 
-[Setup VPN](https://howto.lintel.in/configure-ipsecl2tp-vpn-clients-linux-cli/)
+[Setup VPN](https://gist.github.com/pastleo/aa3a9524664864c505d637b771d079c9)
 
+## Steps
+
+1. yay -S xl2tpd strongswan networkmanager-l2tp
+1. nmcli c add con-name CON_NAME type vpn vpn-type l2tp vpn.data 'gateway=GATEWAY_HOST, ipsec-enabled=yes, ipsec-psk=PRE_SHARED_KEY, password-flags=2, user=USERNAME'
+
+NB. ipsec-psk should be prefixed with 0s and base64 encoded
--- vpn/install.sh
+++ vpn/install.sh
@@ -1,94 +1,24 @@
 #!/bin/bash
 
-yay -S strongswan networkmanager-l2tp xl2tpd bind
+yay -S xl2tpd strongswan networkmanager-l2tp bind
 
 sudo ln -sf $(pwd)/vpn/vpn-start /usr/local/bin/vpn-start
 sudo ln -sf $(pwd)/vpn/vpn-stop /usr/local/bin/vpn-stop
 
-export VPN_SERVER_IP=$(dig office.plingot.com +short)
+# export VPN_SERVER_IP=$(dig office.plingot.com +short)
+export VPN_SERVER_IP="185.96.154.145"
 echo -n "IPSec PSK: "
 read -s PSK
 echo
-export VPN_IPSEC_PSK="${PSK}"
+export VPN_IPSEC_PSK="0s$(echo -n "${PSK}" | base64)"
 echo -n "IPSec User: "
 read -s VPNUSER
 echo
 export VPN_USER="${VPNUSER}"
 echo -n "IPSec Password: "
 read -s VPNPASSWORD
-export VPN_PASSWORD="${VPNPASSWORD}"
+export VPN_PASSWORD="0s$(echo -n "${VPNPASSWORD}" | base64)"
 echo
 
-sudo tee /etc/ipsec.conf > /dev/null <<EOF
-# ipsec.conf - strongSwan IPsec configuration file
-
-# basic configuration
-
-config setup
-  # strictcrlpolicy=yes
-  # uniqueids = no
-
-# Add connections here.
-
-# Sample VPN connections
-
-conn %default
-  ikelifetime=60m
-  keylife=20m
-  rekeymargin=3m
-  keyingtries=1
-  keyexchange=ikev1
-  authby=secret
-  ike=aes128-sha1-modp2048!
-  esp=aes128-sha1-modp2048!
-
-conn plingot
-  keyexchange=ikev1
-  left=%defaultroute
-  auto=add
-  authby=secret
-  type=transport
-  leftprotoport=17/1701
-  rightprotoport=17/1701
-  right=$VPN_SERVER_IP
-EOF
-
-sudo tee /etc/ipsec.secrets > /dev/null <<EOF
-: PSK "$VPN_IPSEC_PSK"
-EOF
-
-sudo chmod 600 /etc/ipsec.secrets
-
-sudo tee /etc/xl2tpd/xl2tpd.conf > /dev/null <<EOF
-[lac plingot]
-lns = $VPN_SERVER_IP
-ppp debug = yes
-pppoptfile = /etc/ppp/options.l2tpd.client
-length bit = yes
-EOF
-
-sudo tee /etc/ppp/options.l2tpd.client > /dev/null <<EOF
-ipcp-accept-local
-ipcp-accept-remote
-refuse-eap
-require-chap
-noccp
-noauth
-mtu 1280
-mru 1280
-noipdefault
-defaultroute
-usepeerdns
-connect-delay 5000
-name $VPN_USER
-password $VPN_PASSWORD
-EOF
-
-sudo chmod 600 /etc/ppp/options.l2tpd.client
-
-sudo systemctl enable strongswan
-sudo systemctl enable strongswan-starter
-sudo systemctl enable xl2tpd
-sudo systemctl start strongswan
-sudo systemctl start strongswan-starter
-sudo systemctl start xl2tpd
+nmcli c add con-name Plingot type vpn vpn-type l2tp vpn.data "gateway=${VPN_SERVER_IP}, ipsec-enabled=yes, ipsec-psk=${VPN_IPSEC_PSK}, password-flags=1, user=${VPN_USER}"
+nmcli con modify Plingot vpn.secrets "password=${VPN_PASSWORD}"
